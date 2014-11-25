5c6a39ca2a6d139228a98565a22c68be060dabab
AirVan21 <airvan21@gmail.com>
2014-11-25 22:10:04 +0300

    Testin Geany editor

---
+++ Automata/Test/FizzBuzz.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// Just Simple FizzBuzz realization
+int main() 
+{
+	for (int i = 1; i <= 100; ++i) {
+		if (i % 3 == 0) {
+			cout << "Fizz";
+		} 
+		if (i % 5 == 0) {
+			cout << "Buzz";
+		}
+		if ((i % 3 != 0) && (i % 5 != 0)) {
+			cout << i;
+		}
+		cout <<  endl; 
+	}
+	return 0;
+}
