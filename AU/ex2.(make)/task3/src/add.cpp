1f47cdc89f772894c15f13c792733336f465786a
AirVan21 <airvan21@gmail.com>
2015-09-17 02:02:20 +0300

    Added const declaration/definition example

---
+++ AU/ex2.(make)/task3/src/add.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+
+const int A1 = 10;
+
+int add(int aNum, int bNum)
+{
+    return aNum + bNum;
+}
---
+++ AU/ex2.(make)/task3/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+
+int main(int argc, char* argv[])
+{
+    using namespace std;
+    cout << add(A1, A2) << endl;
+
+    return 0; 
+}
