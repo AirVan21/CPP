4fe26e78a7e699dc737ceb282fb86a81d365dd9c
AirVan21 <airvan21@gmail.com>
2016-03-16 22:13:39 +0300

    Added functional templates

--- AU/ex9.(algorithm)/algorithm/algo.h
+++ AU/ex9.(algorithm)/algorithm/algo.h
@@ -1,4 +1,42 @@
 #ifndef ALGO_H
 #define ALGO_H
 
+#include <algorithm>
+
+template<class iterator>
+using ivt = typename std::iterator_traits<iterator>::value_type;
+
+template<class T, class comparator = std::less<ivt<T>>>
+int elem_num_in_sorted(T begin, T end, ivt<T> target, comparator test = comparator())
+{
+    auto low = std::lower_bound(begin, end, target, test);
+    auto up  = std::upper_bound(begin, end, target, test);
+
+    return up - low;
+}
+
+template<class Container, class Pred>
+void erase_if(Container& container, Pred pred)
+{
+    container.erase(std::remove_if(container.begin(), container.end(), pred), container.end());
+}
+
+template<class Iterator>
+void print(Iterator begin, Iterator end)
+{
+    while(begin != end) 
+    {
+        std::cout << *begin << " ";
+        begin++;
+    }
+
+    std::cout << std::endl;
+}
+
+template<class Iterator, class Comparator>
+void merge_sort(Iterator begin, Iterator end)
+{
+    printf("%d\n", std::distance(begin, end));
+}
+
 #endif // ALGO_H
